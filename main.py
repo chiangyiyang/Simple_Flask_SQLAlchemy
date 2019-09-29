0e98d93957a4a24e118944ba484e6118d6bbad1d
chiangyiyang <chiangyiyang@gmail.com>
2019-09-29 17:13:41 +0800

    關閉"追蹤修改功能"，以節省資源

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # 設定SQLite資料庫儲存路徑，注意:儲存路徑需要有存取權限
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     "sqlite:///" + os.path.join(basedir, "data.sqlite")
+# 關閉"追蹤修改功能"，以節省資源
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False       
 # 建立資料庫物件
 db = SQLAlchemy(app)
 
