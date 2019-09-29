56b90fdee94e82b85accc7bac8baf52895b2a5d5
chiangyiyang <chiangyiyang@gmail.com>
2019-09-29 17:07:02 +0800

    使用DB Browser for SQLite觀察資料庫內容

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 from main import db
 
 # 建立資料庫
-db.create_all()
+# db.create_all()
+
+# 使用DB Browser for SQLite觀察資料庫內容
+# DB Browser下載位置:
+#   for 32-bit Windows
+#   https://download.sqlitebrowser.org/DB.Browser.for.SQLite-3.11.2-win32.zip
+#   for 64-bit Windows
+#   https://download.sqlitebrowser.org/DB.Browser.for.SQLite-3.11.2-win64.zip
+
